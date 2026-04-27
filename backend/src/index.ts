8101b1d8c77e129ea5b3cb62aa70cd0544ef6965
sanskariladka-jay <jk316875@gmail.com>
2026-04-27 10:06:16 +0530

    commit 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ connectDB();
 IMPORTANT for Render:
 must use process.env.PORT
 */
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
